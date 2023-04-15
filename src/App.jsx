007085ed9dad19f580c2995fdab8fb1baf1d3768
front-ed <edwinschutjes@gmail.com>
2023-04-15 14:07:04 +0200

    add: reload tours function

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,21 @@ const App = () => {
     setTours(newTours);
   };
 
-  useEffect(() => {
+  const fetchTours = async () => {
     try {
-      const fetchTours = async () => {
-        setIsLoading(true);
-        const response = await fetch(url);
-        const tours = await response.json();
-        // console.log(tours);
-        setTours(tours);
-      };
-      fetchTours();
+      setIsLoading(true);
+      const response = await fetch(url);
+      const tours = await response.json();
+      // console.log(tours);
+      setTours(tours);
     } catch (error) {
       console.log(error);
     }
     setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
   }, []);
 
   if (isLoading) {
@@ -36,7 +37,23 @@ const App = () => {
       </main>
     );
   }
-
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tour options left</h2>
+          <button
+            type="button"
+            className="btn"
+            style={{ marginTop: "2rem" }}
+            onClick={fetchTours}
+          >
+            re-load tours
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
