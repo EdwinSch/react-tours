28d861d6223c2290d327a42e5cfe20a006d95596
front-ed <edwinschutjes@gmail.com>
2023-04-15 13:54:54 +0200

    add text length toggle

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 const Tour = ({ id, name, info, image, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} className="img" />
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        <div className="text-container">
+          <p className="display-inline">
+            {readMore ? info : `${info.substring(0, 100)}...`}
+          </p>
+          <button
+            className="info-btn"
+            type="button"
+            onClick={() => setReadMore(!readMore)}
+          >
+            {readMore ? "show less" : "read more"}
+          </button>
+        </div>
+
         <button
           type="button"
           className="btn btn-block delete-btn"
